873af2fbeddec5a5962d97e3e68c1e6c7f3b0c9e
fadlytr <36650539+fadlytr@users.noreply.github.com>
2019-03-29 11:25:58 +0700

    Coba Testing with Mockito
    sudah ada implementasi yang berhasil di class WithdrawalTest (test method setAmount dan setCashDispenser)

--- ATM_New/test/com/appl/atm/model/WithdrawalTest.java
+++ ATM_New/test/com/appl/atm/model/WithdrawalTest.java
@@ -5,18 +5,27 @@
  */
 package com.appl.atm.model;
 
-import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import static org.mockito.Matchers.isA;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
 
 /**
  *
  * @author Gibran
  */
+@RunWith(MockitoJUnitRunner.class)
 public class WithdrawalTest {
     BankDatabase bankDatabase;
     Withdrawal withdrawal;
@@ -78,13 +87,26 @@ public class WithdrawalTest {
          
          assertEquals("Balance Tidak Boleh Minus",0,
                  bankDatabase.getAccount(accountNumber).getTotalBalance(),0);
-     }
+     }     
      @Test
      public void testSetAmount(){
          int amount = this.amountSet;
 //     Withdrawal withdrawal = EasyMock.createMock(Withdrawal.class);
 //     assert Withdrawal
-          withdrawal.setAmount(amount);
-          assertEquals(amount, withdrawal.getAmount());
+//          withdrawal.setAmount(amount);
+//          assertEquals(amount, withdrawal.getAmount());
+        Withdrawal myList;//= Mockito.mock(Withdrawal.class);
+        myList = Mockito.spy(new Withdrawal(this.accountNumber, this.bankDatabase, this.cashDispenser));
+        doNothing().when(myList).setAmount(isA(Integer.class));
+        myList.setAmount(amount);
+        verify(myList).setAmount(amount);
+     }
+     @Test
+     public void testSetCashDispenser(){
+        Withdrawal myList= Mockito.mock(Withdrawal.class);
+//        myList = Mockito.spy(new Withdrawal(this.accountNumber, this.bankDatabase, this.cashDispenser));
+        doNothing().when(myList).setCashDispenser(isA(CashDispenser.class));
+        myList.setCashDispenser(this.cashDispenser);
+        verify(myList).setCashDispenser(this.cashDispenser);
      }
 }
